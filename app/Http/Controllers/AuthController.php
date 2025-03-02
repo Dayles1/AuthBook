4fa50400a731ddb116db47882112531ea3f85062
Dayles1 <sololeveling0025@gmail.com>
2025-03-02 20:08:35 +0500

    +uploadPhoto to Abstract Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,8 +9,9 @@ class AuthController extends Controller
  public function register(Request $request)
  {
     
+    
  }
- public function login(Regquest $request)
+ public function login(Request $request)
  {
 
  }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function uploadPhoto($file, $path='uploads')
+    {
+        $photoName=md5(time().$file->getFilename()).'.'.$file->getClientOriginalExtansion();
+        return $file->storeAs($path,$photoName,'public');
+    }
 }
